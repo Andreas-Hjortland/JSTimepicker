3e1c59450db65147c9521c092c8496db551655c8
Andreas Hjortland <andreas@hjortland.org>
2014-06-07 15:31:48 +0200

    Did some reorganization and decided to use doT instead of handlebars

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,97 +1,118 @@
 
 module.exports = function(grunt) {
-	var buildDir = 'dist/';
-	var srcDir   = 'timepicker/';
-	var jsDir    = srcDir + 'js/';
-	var lessDir  = srcDir + 'less/';
-	var htmlDir  = srcDir;
-	var lessFiles = {};
-	var handlebarsFiles = {};
-	handlebarsFiles[buildDir + "timepickerTemplate.js"] = htmlDir + 'timepicker.html';
-	lessFiles[buildDir + "style.css"] = lessDir + "style.less";
-
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		clean: [buildDir],
+		dirs: {
+			build: 'build',
+			src:   'timepicker',
+			js:    '<%= dirs.src %>/js',
+			less:  '<%= dirs.src %>/less',
+			html:  '<%= dirs.src %>/templates'
+		},
+		srcFiles: {
+			intro:        '<%= dirs.js %>/intro.js',
+			outro:        '<%= dirs.js %>/outro.js',
+			htmlTemplate: '<%= dirs.html %>/timepicker.html',
+			mainJS:       '<%= dirs.js %>/timepicker.js',
+			mainLess:     '<%= dirs.less %>/style.less',
+			gruntfile:    'Gruntfile.js'
+		},
+		dstFiles: {
+			concatJS: '<%= dirs.build %>/<%= pkg.name %>.js',
+			minJS:    '<%= dirs.build %>/<%= pkg.name %>.min.js',
+			css:      '<%= dirs.build %>/<%= pkg.name %>.css',
+			htmlJS:   '<%= dirs.build %>/<%= pkg.name %>.html.js',
+		},
+		clean: ['<%= dirs.build %>'],
 		concat: {
-			dist: {
+			build: {
+				options: {
+					process: function(src, filepath) {
+						return src.replace(/<%=([^%]*)%>/, function(match, p1, offset, string) {
+							return grunt.config.get(p1.trim());
+						});
+					}
+				},
 				src: [
-					buildDir + '',
-					jsDir + 'timepicker.js',
-					jsDir + 'main.js'
+					'<%= srcFiles.intro %>',
+					'<%= dstFiles.htmlJS %>',
+					'<%= jshint.rest %>',
+					'<%= srcFiles.outro %>'
 				],
-				dest: buildDir + '<%= pkg.name %>.js'
+				dest: '<%= dstFiles.concatJS %>',
+				nonull: true
 			}
 		},
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
 			},
 			build: {
-				src: buildDir + '<%= pkg.name %>.min.js',
-				dest: buildDir + '<%= pkg.name %>.min.js'
+				src:  '<%= concat.build.dest %>',
+				dest: '<%= dstFiles.minJS %>'
 			}
 		},
 		jshint: {
+			gruntfile: ['<%= srcFiles.gruntfile %>'],
+			rest: ['<%= srcFiles.mainJS %>']
+		},
+		less: {
+			build: {
+				options: {
+					compress: true,
+					cleancss: true,
+					ieCompat: true,
+				},
+				files: {
+					'<%= dstFiles.css %>': '<%= srcFiles.mainLess %>'
+				}
+			}
+		},
+		dot: {
+			compile: {
+				options: {
+					variable: 'template',
+					requirejs: false,
+					node:      false
+
+				},
+				src:  ['<%= srcFiles.htmlTemplate %>'],
+				dest: '<%= dstFiles.htmlJS %>'
+			}
+		},
+		watch: {
 			gruntfile: {
-				files: [
-					'Gruntfile.js'
-				]
+				files: ['<%= srcFiles.gruntfile %>'],
+				tasks: ['clean', 'default']
 			},
-			rest: {
-				files: [
-					jsDir + '**/*.js'
-				]
+			js: {
+				files: ['<%= dirs.js %>/**/*.js'],
+				tasks: ['js']
+			},
+			css: {
+				files: ['<%= dirs.less %>/**/*.less'],
+				tasks: ['css']
+			},
+			html: {
+				files: ['<%= dirs.html %>/**/*.dot'],
+				tasks: ['compileHB', 'js']
 			}
 		}
-//		less: {
-//			dist: {
-//				options: {
-//					compress: true,
-//					cleancss: true,
-//					ieCompat: true,
-//
-//				},
-//				files: lessFiles
-//			}
-//		},
-//		handlebars: {
-//			compile: {
-//				options: {
-//					namespace: "TimepickerHTML"
-//				},
-//				files: [
-//					''
-//				]
-//			}
-//		},
-//		watch: {
-//			js: {
-//				files: [srcDir + '**/*.js'],
-//				tasks: ['js']
-//			},
-//			css: {
-//				files: [srcDir + '**/*.css', srcDir + '**/*.less'],
-//				tasks: ['css']
-//			},
-//			html: {
-//				files: [srcDir + '**/*.html', srcDir + '**/*.hbs'],
-//				tasks: ['compileHB', 'js']
-//			}
-//		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-handlebars');
+	grunt.loadNpmTasks('grunt-dot-compiler');
 
 	// Default task(s).
-	grunt.registerTask('default', ['js', 'css']);
+	grunt.registerTask('default', ['html', 'js', 'css']);
 	grunt.registerTask('js', ['jshint', 'concat', 'uglify']);
-	grunt.registerTask('css', ['less:dist']);
+	grunt.registerTask('css', ['less:build']);
+	grunt.registerTask('html', ['dot']);
 };
